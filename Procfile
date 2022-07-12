2f77b90a006907c57f08cf2c714bf67c594482d3
Shyam Mistry <sam_for_u@live.com>
2022-07-12 13:43:16 -0400

    Heroku deployment

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn save_soil3.wsgi
---
+++ tests/save_soil3/test_views.py
@@ -0,0 +1,6 @@
+def test_index_ok(client):
+    # Make a GET request to / and store the response object
+    # using the Django test client.
+    response = client.get("/")
+    # Assert that the status_code is 200 (OK)
+    assert response.status_code == 200
