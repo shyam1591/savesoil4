6f4db063f6ac5a1f728e4a07840738849caa2c72
Shyam Mistry <sam_for_u@live.com>
2022-07-18 09:24:24 -0400

    Added html files

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from django.db.models import Count
+from . import models
 
 # Create your views here.
+def home(request):
+    """Blog home page"""
+
+    all_topics = models.Topic.objects.order_by()
+    # allpost = models.Post.objects.all().annotate(commentcount=Count("topics"))
+
+    context = {"all_topics": all_topics}
+
+    return render(request, "blog/home.html", context)
